d0b43347cbdfb5be48ea298db35ca4a54b71604a
riccardo <riccardodeidda25@gmail.com>
2025-10-17 14:34:44 +0200

    started implementing the map

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 class City:
-    def __init__(self, name:str, connected_cities: dict):
+    def __init__(self, name:str, connected_cities: dict[object, float]):
         self.name = name
         self.connected_cities = connected_cities
